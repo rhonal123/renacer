304445c7c146e31d708c69cd535445329675afa6
rhonal <rhonalchirinos@gmail.com>
2017-04-29 08:50:28 -0430

    actualizar los cobradores

--- db/migrate/20170419231057_add_cobrador_id_to_contrato.rb
+++ db/migrate/20170419231057_add_cobrador_id_to_contrato.rb
@@ -2,5 +2,17 @@ class AddCobradorIdToContrato < ActiveRecord::Migration[5.0]
   def change
   	rename_column :contratos, :cobrador, :cobrador_old
     add_reference :contratos, :cobrador, foreign_key: true
+    execute <<-SQL
+    	update contratos set cobrador_old = 'NO-DEFINE' where cobrador_old is null or LENGTH(trim(cobrador_old))  = 0
+    SQL
+		
+		cobradores_old = Contrato.uniq(:cobrador_old).pluck(:cobrador_old)
+		cobradores_old.each_with_index do |cobrador_old,index|
+			cobrador = Cobrador.create(identidad: "V-000000#{index}",nombre: cobrador_old)
+			if cobrador.created_at?
+				Contrato.where(cobrador_old: cobrador_old).update_all(cobrador_id: cobrador.id)				
+			end 
+		end 
+		
   end
 end
