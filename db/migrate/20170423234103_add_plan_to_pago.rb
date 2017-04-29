6e5b32c20ed059b3e8064b3d6c7b746a63b17f64
rhonal <rhonalchirinos@gmail.com>
2017-04-29 08:50:05 -0430

    actualizar el plan_id para el pago

--- db/migrate/20170423234103_add_plan_to_pago.rb
+++ db/migrate/20170423234103_add_plan_to_pago.rb
@@ -1,5 +1,8 @@
 class AddPlanToPago < ActiveRecord::Migration[5.0]
   def change
     add_reference :pagos, :plan, foreign_key: true
+    execute <<-SQL
+      update pagos p set plan_id = (select plan_id from contratos where p.contrato_id = id ) 
+    SQL
   end
 end
