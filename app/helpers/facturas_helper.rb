72e502c8f483a307c17119a4cbcabc16b56d302f
rhonal <rhonalchirinos@gmail.com>
2017-05-20 08:05:43 -0430

    mejoras helper facturacion

--- app/helpers/facturas_helper.rb
+++ app/helpers/facturas_helper.rb
@@ -1,2 +1,14 @@
 module FacturasHelper
+
+	def codigo_factura(id)
+		s = "000000#{id}" 
+		s[s.size-6, s.size]
+	end 
+
+
+	def codigo_detalle(id)
+		s = "000#{id}" 
+		s[s.size-4, s.size]
+	end 
+
 end
