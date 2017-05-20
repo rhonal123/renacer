db4f9696db67cbaa1c0fa1356983d7ae8a1f89e7
rhonal <rhonalchirinos@gmail.com>
2017-05-20 11:12:17 -0430

    Listado de contratos Cobrador

--- app/helpers/cobradores_helper.rb
+++ app/helpers/cobradores_helper.rb
@@ -1,2 +1,6 @@
 module CobradoresHelper
+	def contratos_cobrador cobrador 
+		contratos = cobrador.contratos.joins(:cliente).pluck(:id,:estado,Cliente.arel_table[:nombres],Cliente.arel_table[:apellidos])
+		render('cobradores/contratos', cobrador: cobrador,contratos: contratos )
+	end 
 end
