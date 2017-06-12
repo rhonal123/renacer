a1a7fa4130a1b35ab5e165d133da73e38f8e1ff4
rhonal <rhonalchirinos@gmail.com>
2017-06-11 22:50:41 -0430

    mostrar plan en listado de contratos asignados a un cobrador

--- app/helpers/cobradores_helper.rb
+++ app/helpers/cobradores_helper.rb
@@ -1,6 +1,10 @@
 module CobradoresHelper
 	def contratos_cobrador cobrador 
-		contratos = cobrador.contratos.joins(:cliente).pluck(:id,:estado,Cliente.arel_table[:nombres],Cliente.arel_table[:apellidos])
+		contratos = cobrador.
+			contratos.
+			joins(:cliente,:plan).
+			order(Plan.arel_table[:nombre]).
+			pluck(:id,:estado,Cliente.arel_table[:nombres],Cliente.arel_table[:apellidos],Plan.arel_table[:nombre])
 		render('cobradores/contratos', cobrador: cobrador,contratos: contratos )
 	end 
 end
