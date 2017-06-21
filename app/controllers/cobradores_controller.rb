62b325ad1a07c2faf142efc7ef3e5e2460387c78
Rhonal <rhonalchirinos@gmail.com>
2017-06-21 16:41:13 -0400

    cobradores test

--- app/controllers/cobradores_controller.rb
+++ app/controllers/cobradores_controller.rb
@@ -48,7 +48,7 @@ class CobradoresController < ApplicationController
   def destroy
     begin
       @cobrador.destroy
-      redirect_to cobradores_url, notice: "#{@cobrador.nombres} fue Eliminado."
+      redirect_to cobradores_url, notice: "#{@cobrador.nombre} fue Eliminado."
     rescue ActiveRecord::InvalidForeignKey => e
       flash[:error]  = "No Puedes Eliminar este Cobrador, esta siendo utilizado por contratos."
       redirect_to cobradores_url 
--- test/controllers/cobradores_controller_test.rb
+++ test/controllers/cobradores_controller_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class CobradoresControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @cobrador = cobradores(:one)
+    sign_in usuarios(:one)
   end
 
   test "should get index" do
@@ -17,7 +20,12 @@ class CobradoresControllerTest < ActionDispatch::IntegrationTest
 
   test "should create cobrador" do
     assert_difference('Cobrador.count') do
-      post cobradores_url, params: { cobrador: { identidad: @cobrador.identidad, nombre: @cobrador.nombre } }
+      post cobradores_url, params: { 
+        cobrador: { 
+          identidad: "V-7894587", 
+          nombre: "Sou Camejo" 
+        }
+      }
     end
 
     assert_redirected_to cobrador_url(Cobrador.last)
@@ -40,9 +48,8 @@ class CobradoresControllerTest < ActionDispatch::IntegrationTest
 
   test "should destroy cobrador" do
     assert_difference('Cobrador.count', -1) do
-      delete cobrador_url(@cobrador)
+      delete cobrador_url(cobradores(:cobrador_delete))
     end
-
     assert_redirected_to cobradores_url
   end
 end
