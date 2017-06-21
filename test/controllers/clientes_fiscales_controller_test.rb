f8183b55c282fb9604bfa51689d7ffcd9282e4b9
Rhonal <rhonalchirinos@gmail.com>
2017-06-21 16:37:10 -0400

    tes cliente fiscales

--- test/controllers/clientes_fiscales_controller_test.rb
+++ test/controllers/clientes_fiscales_controller_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class ClientesFiscalesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @cliente_fiscal = clientes_fiscales(:one)
+    sign_in usuarios(:one)
   end
 
   test "should get index" do
@@ -17,9 +20,15 @@ class ClientesFiscalesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create cliente_fiscal" do
     assert_difference('ClienteFiscal.count') do
-      post clientes_fiscales_url, params: { cliente_fiscal: { direccion: @cliente_fiscal.direccion, identidad: @cliente_fiscal.identidad, nombres: @cliente_fiscal.nombres } }
+      post clientes_fiscales_url, params: { 
+        cliente_fiscal: { 
+          direccion: "av venezuela con av leopoldo, barqusimeto estado lara", 
+          identidad: "V-17845878", 
+          nombres:  "Joel Torrez",
+          telefono: "0251-4587945"
+          } 
+      }
     end
-
     assert_redirected_to cliente_fiscal_url(ClienteFiscal.last)
   end
 
